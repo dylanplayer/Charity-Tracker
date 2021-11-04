fe14af732f392cf5acc7d54e794f61d3a092b60d
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2021-11-03 21:22:46 -0700

    Started HTML

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask import Flask, render_template, url_for
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
+
+host = os.environ.get('MONGODB_URI')
+client = MongoClient()
+db = client.get_database('charity-tracker')
+users = db.users
+charities = db.charities
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
