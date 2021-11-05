d4fbfce6a524132036df2f02b901967a21c7eef0
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2021-11-04 17:12:06 -0700

    Added Register Page

--- app.py
+++ app.py
@@ -19,5 +19,9 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/register/', methods=['GET',])
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
