5e5665b24d711c9687255412833ee2e88a8ba193
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2021-11-15 18:03:40 -0800

    Finished MVP

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, make_response, session
 from pymongo import MongoClient
-from datetime import datetime
+from datetime import datetime, timedelta
 from bson import ObjectId
 import bcrypt
 import os
@@ -54,7 +54,7 @@ def register():
         users.insert_one(user)
         session['email'] = user['email']
         session['password'] = user['password']
-        return redirect('/dashboard')
+        return redirect('/dashboard/')
     else:
         return render_template('register.html')
 
@@ -64,8 +64,39 @@ def dashboard():
         email = session['email']
         password = session['password']
         user = users.find_one({"$and": [{'email': email}, {'password': password}]})
-        recent_donations=[]
         if user:
+            recent_donations = []
+            for donation_id in user['donations']:
+                donation = donations.find_one({'_id': donation_id})
+                charity = charities.find_one({'_id': ObjectId(donation['charity_id'])})
+                donation['total_impact'] = int(donation['amount']) * int(charity['impact_per_dollar'])
+                recent_donations.append(
+                    {
+                        'donation': donation,
+                        'charity': charity,
+                    }
+                )
+
+            recent_donations.reverse()
+            
+            user['lifetime'] = 0
+            for donation in recent_donations:
+                user['lifetime'] += float(donation['donation']['amount'])
+            
+            user['year_to_date'] = 0
+            for donation in recent_donations:
+                now = datetime.now()
+                one_year_ago = now - timedelta(days=365)
+                if donation['donation']['created_at'] >= one_year_ago:
+                    user['year_to_date'] += float(donation['donation']['amount'])
+
+            user['month_to_date'] = 0
+            for donation in recent_donations:
+                now = datetime.now()
+                one_month_ago = now - timedelta(days=31)
+                if donation['donation']['created_at'] >= one_month_ago:
+                    user['month_to_date'] += float(donation['donation']['amount'])
+
             return render_template('dashboard.html', user=user, recent_donations=recent_donations)
         else:
             return redirect('/login')
@@ -75,7 +106,10 @@ def dashboard():
 @app.route('/charity/<id>', methods=['GET'])
 def get_charity(id):
     charity = charities.find_one({'_id': ObjectId(id)})
-    return render_template('charity.html', charity=charity) 
+    email = session['email']
+    password = session['password']
+    user = users.find_one({"$and": [{'email': email}, {'password': password}]})
+    return render_template('charity.html', charity=charity, user=user) 
 
 @app.route('/charity/', methods=['POST'])
 def create_charity():
@@ -101,6 +135,30 @@ def get_create_charity_form():
 def charitiesList():
     if request.method == 'GET':
         return render_template('charities.html', charities=charities.find())
+
+@app.route('/donate/', methods=['POST'])
+def donate():
+    if request.method == 'POST':
+        email = session['email']
+        password = session['password']
+        user = users.find_one({"$and": [{'email': email}, {'password': password}]})
+        donation = {
+            'charity_id': request.form.get('charity'),
+            'user_id': user['_id'],
+            'amount': request.form.get('amount'),
+            'created_at': datetime.now(),
+        }
+        donations.insert_one(donation)
+        donation = donations.find_one({'created_at': donation['created_at']})
+        users.update_one(
+            {'_id': user['_id']},
+            {'$push': {'donations': donation['_id']}}
+        )
+        charities.update_one(
+            {'_id': donation['charity_id']},
+            {'$push': {'donations': donation['_id']}}
+        )
+        return redirect('/dashboard/')
 
 if __name__ == '__main__':
     app.run(debug=True)
